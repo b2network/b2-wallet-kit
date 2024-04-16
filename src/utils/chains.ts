736e968f0a2b60e819714d2d21a0c2901a460990
lance10030 <lance10030@163.com>
2024-04-16 13:59:48 +0800

    feat: add chain

--- src/utils/chains.ts
+++ src/utils/chains.ts
@@ -72,3 +72,27 @@ export const b2Testnet = defineChain<Chain, ChainFormatters | undefined>({
     },
   },
 })
+export const b2Network = defineChain<Chain, ChainFormatters | undefined>({
+  id: 223,
+  name: 'B2Network',
+  network: 'B2Network',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Bitcoin',
+    symbol: 'BTC',
+  },
+  blockExplorers: {
+    default: {
+      name: 'B2Network',
+      url: ' https://explorer.bsquared.network/'
+    }
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.bsquared.network/'],
+    },
+    public: {
+      http: ['https://rpc.bsquared.network/'],
+    },
+  },
+})
