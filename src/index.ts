a4836cd5a0ae53e095c1e9d0160729f7111a3ffe
lance10030 <lance10030@163.com>
2024-07-02 12:00:12 +0800

    feat: add coin98

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './context';
 
 export * from './utils/chains'
 export * from './utils/wallet/tomoBtcConnector'
+export * from './utils/wallet/Coin98BtcConnector'
 export * from './utils/signedMsgAdapter'
 export * from './hooks'
 export * from './utils/wallet/types'
--- src/utils/wallet/index.ts
+++ src/utils/wallet/index.ts
@@ -5,11 +5,25 @@ import iconGate from '../../imgs/icon_gate.svg'
 import iconTomo from '../../imgs/tomo.png'
 import iconUnisat from '../../imgs/icon_unisat.svg'
 import iconBybit from '../../imgs/bybit.png'
+import iconCoin98 from '../../imgs/icon_coin98.svg'
 export type InstalledObj = Record<WalletTypes, boolean>
 export type EvmConnectorName = 'gate' | 'metamask' | 'bybit' | 'okx'
 
-export const BtcWalletArr = [WalletTypes.WALLET_OKX_BTC, WalletTypes.WALLET_TOMO_BTC, WalletTypes.WALLET_UNISAT, WalletTypes.WALLET_BYBIT_BTC]
-export const EvmWalletArr = [WalletTypes.WALLET_GATE, WalletTypes.WALLET_METAMASK, WalletTypes.WALLET_BYBIT_EVM, WalletTypes.WALLET_OKX_EVM, WalletTypes.WALLET_TOMO_EVM]
+export const BtcWalletArr = [
+  WalletTypes.WALLET_OKX_BTC,
+  WalletTypes.WALLET_TOMO_BTC,
+  WalletTypes.WALLET_UNISAT,
+  WalletTypes.WALLET_BYBIT_BTC,
+  WalletTypes.WALLET_COIN98_BTC
+]
+export const EvmWalletArr = [
+  WalletTypes.WALLET_GATE,
+  WalletTypes.WALLET_METAMASK,
+  WalletTypes.WALLET_BYBIT_EVM,
+  WalletTypes.WALLET_OKX_EVM,
+  WalletTypes.WALLET_TOMO_EVM,
+  WalletTypes.WALLET_COIN98_EVM
+]
 export const defaultInstalledMap: InstalledObj = {
   metamask: false,
   unisat: false,
@@ -19,7 +33,9 @@ export const defaultInstalledMap: InstalledObj = {
   tomo_evm: false,
   tomo_btc: false,
   bybit_evm: false,
-  bybit_btc: false
+  bybit_btc: false,
+  coin98_btc: false,
+  coin98_evm: false
 }
 
 
@@ -47,6 +63,9 @@ export const evmWalletNameTransformer = (wallet: string): WalletTypes | undefine
   if (wallet?.toLocaleLowerCase().includes('tomo')) {
     name = WalletTypes.WALLET_TOMO_EVM
   }
+  if (wallet?.toLocaleLowerCase().includes('coin98')) {
+    name = WalletTypes.WALLET_COIN98_EVM
+  }
   return name
 }
 
@@ -63,6 +82,9 @@ export const btcWalletNameTransformer = (wallet: string): WalletTypes | undefine
   }
   if (wallet?.toLocaleLowerCase().includes('bybit')) {
     name = WalletTypes.WALLET_BYBIT_BTC
+  }
+  if (wallet?.toLocaleLowerCase().includes('coin98')) {
+    name = WalletTypes.WALLET_COIN98_BTC
   }
   return name
 }
@@ -86,6 +108,10 @@ export const checkWalletInstall = (i: InstalledObj): InstalledObj => {
     installed.tomo_btc = true;
     installed.tomo_evm = true
   }
+  if (window.coin98) {
+    installed.coin98_btc = true
+    installed.coin98_evm = true
+  }
   return installed
 }
 export const getBtcWalletName = (wallet: string) => {
@@ -93,6 +119,7 @@ export const getBtcWalletName = (wallet: string) => {
   if (wallet.toLowerCase().includes('okx')) return 'OKX Wallet'
   if (wallet.toLocaleLowerCase().includes('tomo')) return 'Tomo Wallet'
   if (wallet.toLocaleLowerCase().includes('bybit')) return 'Bybit Wallet'
+  if (wallet.toLocaleLowerCase().includes('coin98')) return 'Coin98 Wallet'
   return ''
 }
 export const WalletIconConf = [
@@ -110,5 +137,8 @@ export const WalletIconConf = [
   },
   {
     name: 'gate', icon: iconGate
+  },
+  {
+    name: 'coin98', icon: iconCoin98
   }
 ]
