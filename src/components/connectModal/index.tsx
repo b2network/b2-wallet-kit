7c9ddebcb091f908d042ed9a6aa81c65557c215b
lance10030 <lance10030@163.com>
2025-01-22 15:44:37 +0800

    feat: text change

--- src/components/connectModal/index.tsx
+++ src/components/connectModal/index.tsx
@@ -167,7 +167,7 @@ const ConnectModal = ({ collection }: { collection: WalletCollection }) => {
       </div>
       <div className={styles.privacy}>
         <span>By logging in I agree to the </span>
-        <a className={styles.link} href="https://www.bsquared.network/terms-of-service">Terms of Service</a> <span> and</span> <a className={styles.link} href="https://www.bsquared.network/privacy-policy">Privacy Policy</a>
+        <a className={styles.link} href="https://www.bsquared.network/terms-of-service">Terms of Service</a> <span> and </span> <a className={styles.link} href="https://www.bsquared.network/privacy-policy">Privacy Policy</a>
       </div>
     </Modal>
   )
